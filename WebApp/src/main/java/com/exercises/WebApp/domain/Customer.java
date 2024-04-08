1ddfa0e76e973af5542daed2e250224edc9b8826
fanus abraha <fanus.abraha@yahoo.com>
2024-04-08 20:27:00 +0200

    Initial commit

---
+++ WebApp/src/main/java/com/exercises/WebApp/domain/Customer.java
@@ -0,0 +1,11 @@
+package com.exercises.WebApp.domain;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class Customer {
+    private String name;
+    private String userType;
+    
+
+}

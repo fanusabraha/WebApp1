bcb62eb7ee9752f7222c9b163459753dc31dd7ae
fanus abraha <fanus.abraha@yahoo.com>
2024-04-08 22:06:40 +0200

    Adding html and thymeleaf

--- WebApp/src/main/java/com/exercises/WebApp/controller/AdminClassController.java
+++ WebApp/src/main/java/com/exercises/WebApp/controller/AdminClassController.java
@@ -3,15 +3,24 @@ package com.exercises.WebApp.controller;
 import com.exercises.WebApp.domain.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
-@RequestMapping()
+@RequestMapping("")
 public class AdminClassController {
-    @Autowired
-    Customer customer;
-    @RequestMapping("loggin")
-    public String getCustomerRole (){
-    new Customer("Andreas","Teacher","ADMIN");
+
+    @GetMapping("/loggin")
+    public String getCustomerRole (ModelMap modelMap){
+        List<Customer> customers= new ArrayList<>();
+        customers.add(new Customer("Andreas","Teacher","ADMIN"));
+        customers.add(new Customer("olaf","Coordinator","USER"));
+        customers.add(new Customer("olga","Manager","USER"));
+        customers.add(new Customer("Sylvia","Boss","USER"));
+        modelMap.put("customername",customers);
         return"if_else";}
 }
--- WebApp/src/main/java/com/exercises/WebApp/domain/Customer.java
+++ WebApp/src/main/java/com/exercises/WebApp/domain/Customer.java
@@ -2,7 +2,7 @@ package com.exercises.WebApp.domain;
 
 import org.springframework.stereotype.Repository;
 
-@Repository
+
 public class Customer {
     private String name;
     private String userType;

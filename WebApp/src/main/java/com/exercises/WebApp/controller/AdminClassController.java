07af38d73d914b1617127f6c9db96eb3987b4463
fanus abraha <fanus.abraha@yahoo.com>
2024-04-08 20:41:57 +0200

    Adding Customer

--- WebApp/src/main/java/com/exercises/WebApp/controller/AdminClassController.java
+++ WebApp/src/main/java/com/exercises/WebApp/controller/AdminClassController.java
@@ -1,10 +1,17 @@
 package com.exercises.WebApp.controller;
 
+import com.exercises.WebApp.domain.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping
+@RequestMapping()
 public class AdminClassController {
-
+    @Autowired
+    Customer customer;
+    @RequestMapping("loggin")
+    public String getCustomerRole (){
+    new Customer("Andreas","Teacher","ADMIN");
+        return"if_else";}
 }

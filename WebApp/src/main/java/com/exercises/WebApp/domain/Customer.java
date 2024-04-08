f9f938766e756f0ae7cc6fe75b95d360b90d69e0
fanus abraha <fanus.abraha@yahoo.com>
2024-04-08 20:32:53 +0200

    Update Customer.java

--- WebApp/src/main/java/com/exercises/WebApp/domain/Customer.java
+++ WebApp/src/main/java/com/exercises/WebApp/domain/Customer.java
@@ -6,6 +6,35 @@ import org.springframework.stereotype.Repository;
 public class Customer {
     private String name;
     private String userType;
-    
+    private String role;
 
+    public Customer(String name, String userType, String role) {
+        this.name = name;
+        this.userType = userType;
+        this.role = role;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
